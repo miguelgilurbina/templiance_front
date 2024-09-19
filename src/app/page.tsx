d13d0544648ab2b6e2bfaaf85618df99822d413f
MrChocolate <mrchocolatepr@gmail.com>
2024-09-18 21:48:34 -0300

    ADD: Adding benefitSection component to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Navbar from "../app/general/Navbar";
 import Footer from "../app/general/Footer";
 import Hero from "../app/components/Hero";
 import PDFPreviewCards from "./components/pdf-preview-cards";
+import BenefitsSection from "./components/BenifitsSection";
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <Hero />
+      <BenefitsSection />
       <PDFPreviewCards />
       <Footer />
     </div>
